61619331ca2dd5576ff87bead526b89a63ce5bf6
Benjamin Fabri <contact@bfabri.me>
2023-06-26 01:15:36 +0200

    Add project section

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bfabri.me",
   outDir: "public",
   publicDir: "static",
-  integrations: [tailwind(), preact(), sitemap()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    preact(),
+    sitemap(),
+    mdx(),
+  ],
 });
---
+++ src/content/config.ts
@@ -0,0 +1,34 @@
+import { z, defineCollection } from "astro:content";
+
+const projectCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    order: z.number(),
+    name: z.string(),
+    description: z.string(),
+    resume: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    techStack: z.object({
+      frontend: z.array(z.string()).optional(),
+      extension: z.array(z.string()).optional(),
+      projet: z.array(z.string()).optional(),
+    }),
+    links: z.object({
+      website: z.string().optional(),
+      extensionStore: z.string().optional(),
+      frontend: z.string().optional(),
+      backend: z.string().optional(),
+      extension: z.string().optional(),
+      projet: z.string().optional(),
+    }),
+    link: z.string(),
+    tag: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  project: projectCollection,
+};
