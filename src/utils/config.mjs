77c9f166c8b6d57061851a57a907c5e7b5aac1fa
Benjamin Fabri <contact@bfabri.me>
2023-06-28 00:40:35 +0200

    Fix dark/light mode with system themes

--- src/utils/config.mjs
+++ src/utils/config.mjs
@@ -1,6 +1,6 @@
 const CONFIG = {
-  defaultTheme: "light", // Values: "system" | "light" | "dark"
-  language: "fr",
+    defaultTheme: "system", // Values: "system" | "light" | "dark"
+    language: "fr",
 };
 
 export const SITE = { ...CONFIG };
