53ebdd3f145082ce9e3edc21f134a01741a9bc2c
Benjamin Fabri <contact@bfabri.me>
2023-07-05 05:29:51 +0200

    Update project description

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,35 +1,34 @@
 import { z, defineCollection } from "astro:content";
 
 const projectCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    order: z.number(),
-    name: z.string(),
-    description: z.string(),
-    resume: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
+    schema: z.object({
+        draft: z.boolean(),
+        order: z.number(),
+        name: z.string(),
+        description: z.string(),
+        image: z.object({
+            src: z.string(),
+            alt: z.string(),
+        }),
+        techStack: z.object({
+            frontend: z.array(z.string()).optional(),
+            backend: z.array(z.string()).optional(),
+            project: z.array(z.string()).optional(),
+            extension: z.array(z.string()).optional(),
+        }),
+        links: z.object({
+            website: z.string().optional(),
+            extensionStore: z.string().optional(),
+            frontend: z.string().optional(),
+            backend: z.string().optional(),
+            extension: z.string().optional(),
+            project: z.string().optional(),
+        }),
+        link: z.string(),
+        tag: z.array(z.string()),
     }),
-    techStack: z.object({
-      frontend: z.array(z.string()).optional(),
-      backend: z.array(z.string()).optional(),
-      project: z.array(z.string()).optional(),
-      extension: z.array(z.string()).optional(),
-    }),
-    links: z.object({
-      website: z.string().optional(),
-      extensionStore: z.string().optional(),
-      frontend: z.string().optional(),
-      backend: z.string().optional(),
-      extension: z.string().optional(),
-      project: z.string().optional(),
-    }),
-    link: z.string(),
-    tag: z.array(z.string()),
-  }),
 });
 
 export const collections = {
-  project: projectCollection,
+    project: projectCollection,
 };
