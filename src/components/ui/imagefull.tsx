640a4a133c9e1aa4684ddff42eaecf95ea958e28
Benjamin Fabri <contact@bfabri.me>
2023-07-06 22:32:34 +0200

    Add fullsize toggle on image component

---
+++ src/components/ui/imagefull.tsx
@@ -0,0 +1,47 @@
+import { h, Component } from "preact";
+
+type ImageProps = {
+    src: string;
+    alt: string;
+    class: string;
+};
+
+type ImageState = {
+    isFullsize: boolean;
+};
+
+export default class Imagefull extends Component<ImageProps, ImageState> {
+    state: ImageState = { isFullsize: false };
+
+    handleImageClick = () => {
+        this.setState((prevState) => ({ isFullsize: !prevState.isFullsize }));
+    };
+
+    render({ src, alt, class: className }: ImageProps, { isFullsize }: ImageState) {
+        const fullsizeImageClasses = [
+            "fixed",
+            "top-1/2",
+            "left-1/2",
+            "w-4/5",
+            "h-4/5",
+            "z-50",
+            "object-contain",
+            "transform",
+            "translate-x-[-50%]",
+            "translate-y-[-50%]",
+        ].join(" ");
+
+        const overlayClasses = ["fixed", "inset-0", "z-40", "bg-black", "bg-opacity-80"].join(" ");
+
+        const baseClasses = `${className} fullsize-image ${isFullsize ? "" : "object-cover object-left-top"}`;
+        const imgClasses = `${baseClasses} ${isFullsize ? fullsizeImageClasses : ""}`;
+        const overlayClass = `overlay ${isFullsize ? overlayClasses : ""}`;
+
+        return (
+            <>
+                <img src={src} alt={alt} class={imgClasses} onClick={this.handleImageClick} role="button" />
+                <div class={overlayClass} onClick={this.handleImageClick} aria-hidden="true"></div>
+            </>
+        );
+    }
+}
