fc289f690c477aeac381377078d5e9d72509adc2
Benjamin Fabri <contact@bfabri.me>
2023-08-30 02:42:03 +0200

    Fix safari scroll lock

--- src/components/ui/imagefull.tsx
+++ src/components/ui/imagefull.tsx
@@ -10,15 +10,33 @@ type ImageState = {
     isFullsize: boolean;
 };
 
+let scrollPosition = 0;
+
+const lockBodyScroll = () => {
+    scrollPosition = window.scrollY;
+    document.body.style.overflow = "hidden";
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${scrollPosition}px`;
+    document.body.style.width = "100%";
+};
+
+const unlockBodyScroll = () => {
+    document.body.style.removeProperty("overflow");
+    document.body.style.removeProperty("position");
+    document.body.style.removeProperty("top");
+    document.body.style.removeProperty("width");
+    window.scrollTo(0, scrollPosition);
+};
+
 export default class Imagefull extends Component<ImageProps, ImageState> {
     state: ImageState = { isFullsize: false };
 
     handleImageClick = () => {
         this.setState((prevState) => {
             if (prevState.isFullsize) {
-                document.body.style.overflow = "";
+                unlockBodyScroll();
             } else {
-                document.body.style.overflow = "hidden";
+                lockBodyScroll();
             }
             return { isFullsize: !prevState.isFullsize };
         });
