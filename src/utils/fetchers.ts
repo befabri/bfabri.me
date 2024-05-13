5a8172efcf1b5de866ec05ec712bfed8f297f74b
Benjamin Fabri <bfabri@pm.me>
2024-05-13 22:41:30 +0200

    Move sortItem, getStaticPaths to fetchers

--- src/utils/fetchers.ts
+++ src/utils/fetchers.ts
@@ -5,3 +5,22 @@ export async function getProjects() {
         .filter(project => !project.data.draft)
     return projects;
 }
+
+export async function getProjectsSorted() {
+    const projectEntries = await getCollection("project");
+    const projects = projectEntries.map((entry) => ({
+        id: entry.id,
+        link: entry.data.link,
+        order: entry.data.order,
+    }));
+    const sortedProjects = projects.sort((a, b) => a.order - b.order);
+    return sortedProjects;
+}
+
+export async function getProjectsStaticPaths() {
+    const projectEntries = await getCollection("project");
+    return projectEntries.map((entry) => ({
+        params: { slug: entry.slug },
+        props: { entry },
+    }));
+}
