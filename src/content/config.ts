a2d0506d3203e1b1bc91f095f9643d4214cc26a1
Benjamin Fabri <bfabri@pm.me>
2024-10-15 02:49:39 +0200

    Add 'ClashOfDev' project and removed unused md config field

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,23 +11,11 @@ const projectCollection = defineCollection({
             src: z.string(),
             alt: z.string(),
         }),
-        techStack: z.object({
-            frontend: z.array(z.string()).optional(),
-            backend: z.array(z.string()).optional(),
-            project: z.array(z.string()).optional(),
-            extension: z.array(z.string()).optional(),
-        }),
         links: z.object({
             website: z.string().optional(),
-            extensionStore: z.string().optional(),
-            frontend: z.string().optional(),
-            backend: z.string().optional(),
-            extension: z.string().optional(),
             project: z.string().optional(),
         }),
-        link: z.string(),
         tag: z.array(z.string()),
-        imageFolder: z.string(),
         images: z.array(
             z.object({
                 src: z.string(),
